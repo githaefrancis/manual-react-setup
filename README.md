419573e40162ab96ef7621d17310fd4766c185e4
githaefrancis <mureithigithae@gmail.com>
2024-02-04 00:51:56 +0300

    ch(Setup) - setup react app

---
+++ README.md
@@ -0,0 +1,1 @@
+[reference]('https://medium.com/@tharinduit16/react-18-with-webpack-5-project-setup-steps-a93b4e1aaa3b')
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App(){
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
+            <p>
+            Greates App
+            Greates App
+            Greates App
+            Greates App
+            Greates App
+            Greates App
+            Greates App
+            Greates App
+            Greates App
+            </p>
+        </div>
+    )
+}
+
+export default App;
